20154406c26554b1c29ac0cd8c8006540ee5a7e3
Daniel Jang <djang89@gmail.com>
2017-03-26 23:50:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,6 @@ def plotpage():
 
 
 
-if __name__ == '__main__':
-    port=int(os.environ.get("PORT",5000))
-    
-    if port==5000 :
-        app.run(host='0.0.0.0')
-    else:
-        app.run(port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
